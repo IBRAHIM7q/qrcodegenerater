9379c3b1938c79a183b345cd8b5fb6c021e16d20
IBRAHIM7q <alsaadi.aa.ss85@gmail.com>
2025-10-10 23:25:52 +0200

    wmm

---
+++ netlify-lambda-webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  externals: {
+    'node:fs': 'commonjs fs',
+    'node:path': 'commonjs path',
+    'node:url': 'commonjs url',
+    'node:buffer': 'commonjs buffer',
+    'node:stream': 'commonjs stream',
+    'node:util': 'commonjs util',
+    'node:os': 'commonjs os'
+  }
+};
